5381fed31e31879e398dd9f1d19284975b4ff679
Mark Cherrie <mark.cherrie.mc@gmail.com>
2019-09-14 00:26:12 +0100

    data test 3

--- R/JAXAconvertdaily.R
+++ R/JAXAconvertdaily.R
@@ -1,13 +1,18 @@
-#!/bin/bash
 
-cd ../rawdata
+JAXA_convert<-function(){
+system('
+
+mkdir convert
+cd rawdata
 FILES=*
 gzip -d *.gz
 rm *.gz
+
 
 for f in $FILES;
 do
 perl ../perl/JAXAconvertdaily.pl < "$f" > ../convert/$f.txt
 echo "Extracting $f..."
 rm "$f"
-done
+done')
+}
---
+++ R/test.R
@@ -0,0 +1,15 @@
+system('
+
+mkdir convert
+cd rawdata
+FILES=*
+gzip -d *.gz
+rm *.gz
+
+
+for f in $FILES;
+do
+perl ../perl/JAXAconvertdaily.pl < "$f" > ../convert/$f.txt
+echo "Extracting $f..."
+rm "$f"
+done')
