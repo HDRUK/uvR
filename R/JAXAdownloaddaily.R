72a630992b585e697a5bc2cc481f1c87b1aee835
Mark Cherrie <mark.cherrie.mc@gmail.com>
2019-09-14 00:31:01 +0100

    data test 5

--- R/JAXAdownloaddaily.R
+++ R/JAXAdownloaddaily.R
@@ -3,7 +3,6 @@
 ## download JAXA daily file
 
 JAXA_download <-function(date, satellite, UVRtype){
-  dir.create("rawdata")
   date<-gsub("-","", date)
       FTP <- paste0("ftp://apollo.eorc.jaxa.jp/pub/JASMES/Global_05km/", UVRtype, "/daily/", substring(date,1,6), "/")
       file<-paste0("MOD", "02SSH_A", date , "Av1_v601_7200_3601_",UVRtype,"__le.gz")
