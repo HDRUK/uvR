9427f2f714b7f4ee5147135eeac841ead36fbdc7
Mark Cherrie <mark.cherrie.mc@gmail.com>
2019-09-14 14:11:32 +0100

    changed long subset to right way around

--- R/JAXAprocessdaily.R
+++ R/JAXAprocessdaily.R
@@ -10,20 +10,18 @@ dir.create("subset")
 infiles<-list.files("convert", ".txt*")
 change.files <- function(file)
 {
-  mydata = read.csv(paste0("convert/",file),  header=FALSE)
-  colnames(mydata)<-(seq(0, 360, by = 0.05))-0.05
-  colnames(mydata)[1]<-"Latitude"
-  mydata$Latitude<-(mydata$Latitude-90.05)*-1
-  mydata2<-subset(mydata, Latitude<=latitudeN & Latitude>=latitudeS, select=c(Latitude, 0:ncol(mydata)))
-  mydata2$Latitude.1<-NULL
-  mdata <- melt(mydata2, id=c("Latitude"))
-  write.csv(mdata, row.names=FALSE, "convert/temp.csv")
-  mydata3<-read.csv("convert/temp.csv")
-  mydata3$variable<-ifelse(mydata3$variable>180, -360+ mydata3$variable, mydata3$variable)
-  colnames(mydata3) <- c("Latitude", "Longitude", "UV")
-  mydata4<-subset(mydata3, Longitude>=longitudeE & Longitude<=longitudeW, select=c(Latitude, Longitude, UV))
+  original = read.csv(paste0("convert/",file),  header=FALSE)
+  colnames(original)<-(seq(0, 360, by = 0.05))-0.05
+  colnames(original)[1]<-"Latitude"
+  original$Latitude<-(original$Latitude-90.05)*-1
+  latsub<-subset(original, Latitude<=latitudeN & Latitude>=latitudeS, select=c(Latitude, 0:ncol(original)))
+  latsub$Latitude.1<-NULL
+  latsublong <- melt(latsub, id=c("Latitude"))
+  latsublong$variable<-ifelse(latsublong$variable>180, -360+ latsublong$variable, latsublong$variable)
+  colnames(latsublong) <- c("Latitude", "Longitude", "UV")
+  latlongsublong<-subset(latlongsublong, Longitude>=longitudeW & Longitude<=longitudeE, select=c(Latitude, Longitude, UV))
   name<-sub(".txt$","-edit.txt", file)
-  write.csv(mydata4, row.names=FALSE, quote=FALSE, paste0("subset/",name))
+  write.csv(latlongsublong, row.names=FALSE, quote=FALSE, paste0("subset/",name))
   file.remove(paste0("convert/",file))
 }
 
@@ -43,11 +41,12 @@ infiles<-list.files("subset", "-edit.txt*")
 
 # batch
 interpolate<-function(file){
-  data<- read.csv(file = paste0("subset/",file), header = TRUE)
-  data<-na.omit(data)
-  coordinates(data) = ~Longitude + Latitude
+  latlongsublong <- read.csv(file = paste0("subset/",file), header = TRUE)
+  latlongsublong <-na.omit(data)
+  coordinates(latlongsublong) = ~Longitude + Latitude
   x.range <- as.numeric(c(longitudeW, longitudeE))
   y.range <- as.numeric(c(latitudeS,latitudeN))
+
   # Create an empty grid where n is the total number of cells
   grd              <- as.data.frame(spsample(data, "regular", n=90000))
   names(grd)       <- c("X", "Y")
@@ -56,7 +55,7 @@ interpolate<-function(file){
   fullgrid(grd)    <- TRUE  # Create SpatialGrid object
 
   # Interpolate the surface using a power value of 2 (idp=2.0)
-  dat.idw <- idw(UV~1,data,newdata=grd,idp=2.0, na.action=na.omit)
+  dat.idw <- idw(UV~1,latlongsublong,newdata=grd,idp=2.0, na.action=na.omit)
   rasterDF <- raster(dat.idw)
   bb <- extent(longitudeW, longitudeE, latitudeS, latitudeN)
   extent(rasterDF) <- bb
